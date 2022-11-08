32cf8a05891c86d951ad5237ab851a660e5542bb
Yurii-Hubych <yura.ghubych123@gmail.com>
2022-11-08 22:25:15 +0200

    Add project files.

---
+++ SpecFlowProject1/Features/OpenAccount.feature
@@ -0,0 +1,9 @@
+﻿Feature: OpenAccount
+
+A short summary of the feature
+
+@feature1
+Scenario: A new account should be opened successfuly
+	Given XYZ Bank page
+	When click Bank Manager Login, click Open Account, choose Customer name Hermoine Granger, choose Currency Rupee, click Process
+	Then Success alert should appear
---
+++ SpecFlowProject1/StepDefinitions/OpenAccountSteps.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using TechTalk.SpecFlow;
+using Lab_2;
+using SpecFlowProject1.StepDefinitions;
+
+namespace MyNamespace
+{
+    [Binding]
+    public class StepDefinitions:BaseSteps
+    {
+        private OpenAccountPage openAccountPage;
+
+        [Given(@"XYZ Bank page")]
+        public void GivenXYZBankPage()
+        {
+            driver.Url = "https://www.globalsqa.com/angularJs-protractor/BankingProject";
+            openAccountPage = new OpenAccountPage(driver);
+        }
+
+        [When(@"click Bank Manager Login, click Open Account, choose Customer name Hermoine Granger, choose Currency Rupee, click Process")]
+        public void WhenClickBankManagerLoginClickOpenAccountChooseCustomerNameHermoineGrangerChooseCurrencyRupeeClickProcess()
+        {
+            openAccountPage.OpenANewAccount();
+        }
+
+        [Then(@"Success alert should appear")]
+        public void ThenSuccessAlertShouldAppear()
+        {
+            openAccountPage.CheckThatAlertContainsText("Account created successfully");
+        }
+    }
+}
